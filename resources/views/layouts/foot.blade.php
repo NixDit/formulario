c808007efd57e0b4eb1573904b1fc71b8fd191c6
JPablo <juan@teknik.mx>
2021-10-07 22:31:54 -0500

    Init project

---
+++ resources/views/layouts/foot.blade.php
@@ -0,0 +1,27 @@
+<!-- Vendor -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/jquery.appear/jquery.appear.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/jquery.cookie/jquery.cookie.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/jquery.validation/jquery.validate.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/jquery.gmap/jquery.gmap.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/lazysizes/lazysizes.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/isotope/jquery.isotope.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/owl.carousel/owl.carousel.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/magnific-popup/jquery.magnific-popup.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/vide/jquery.vide.min.js?v='.rand()) }}"></script>
+    <script src="{{ asset('vendor/vivus/vivus.min.js?v='.rand()) }}"></script>
+
+    <!-- Theme Base, Components and Settings -->
+    <script src="{{ asset('js/theme.js?v='.rand()) }}"></script>
+
+    <!-- Theme Custom -->
+    <script src="{{ asset('js/custom.js?v='.rand()) }}"></script>
+
+    <!-- Theme Initialization Files -->
+    <script src="{{ asset('js/theme.init.js?v='.rand()) }}"></script>
+
+    </body>
+</html>
