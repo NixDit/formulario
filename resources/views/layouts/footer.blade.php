afaeec276484df6ad45b49b3b2aa9482b321aabf
maproderm <maprodermweb@gmail.com>
2025-01-09 11:07:14 -0600

    Update redes sociales & copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,9 +7,9 @@
         </a>
         {{-- <p class="text-4 mb-4">Porto is exclusively available on themeforest.net by <a href="https://themeforest.net/user/okler/" class="text-color-light text-decoration-none" target="_blank">Okler.</a></p> --}}
         <ul class="social-icons social-icons-big social-icons-dark-2">
-            <li class="social-icons-facebook"><a href="http://www.facebook.com/maproderm" target="_blank" title="Facebook Imaq Industrial Maquiladora"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="social-icons-facebook"><a href="https://www.facebook.com/imaqindustrialmaquiladoraosc/" target="_blank" title="Facebook Imaq Industrial Maquiladora"><i class="fab fa-facebook-f"></i></a></li>
             <li class="social-icons-twitter"><a href="http://www.twitter.com/maproderm" target="_blank" title="Twitter Imaq Industrial Maquiladora"><i class="fab fa-twitter"></i></a></li>
-            <li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin Imaq Industrial Maquiladora"><i class="fab fa-linkedin-in"></i></a></li>
+            <li class="social-icons-linkedin"><a href="javascript:;" xtarget="_blank" title="Linkedin Imaq Industrial Maquiladora"><i class="fab fa-linkedin-in"></i></a></li>
         </ul>
         <ul>
             {{-- <p class="text-color-white mb-0 text-2">contacto@industrialmaquiladora.com <br>55 88 48 46 39</p> --}}
@@ -25,7 +25,7 @@
     </div>
     <div class="copyright bg-dark py-3">
         <div class="container text-center py-2">
-            <p class="text-color-white mb-0 text-2">Copyright 2024 - IMAQ Industrial Maquiladora OSC S.A de C.V. - Todos los derechos reservados</p>
+            <p class="text-color-white mb-0 text-2">Copyright 2025 - IMAQ Industrial Maquiladora OSC S.A de C.V. - Todos los derechos reservados</p>
         </div>
     </div>
 </footer>
